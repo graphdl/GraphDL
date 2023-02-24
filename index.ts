0e30c2d78872333ea3d0766d1e7aa8cef4c2b0c5
Nathan Clevenger <nateclev@gmail.com>
2023-02-24 03:50:10 -0600

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,24 @@
+export Type Database = {
+  _id: string
+  _name: string
+  _icon: string
+  _defaultId?: string
+  [string]: Model
+}
+
+
+
+export Type Model = {
+  _id: string
+  _name: string
+  _icon: string
+  
+}
+
+export Type PropertyTypes = 'string' | 'date' |  'datetime' | 'timestamp' | 'markdown' | 'json'
+export Type Property = PropertyTypes | `[${PropertyTypes}]` | `${PropertyTypes}!` 
+
+export const database = init => {
+  const _id = init._id ?? init._name ? slugify(init._name) : Math.random().toString(36).substring(2,7)
+  const { _name, _icon = '■' } = init
+}
