d10ffec4b1c5131df27ebf5c8b5d34155b9a5792
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2023-02-28 02:34:50 -0600

    Cleanup

--- index.ts
+++ index.ts
@@ -1,29 +1,31 @@
-export Type Database = {
+export type Database = {
   _id: string
   _name: string
   _icon: string
   _defaultId?: string
-  [string]: Model
+  [key: string]: string | undefined | Model
 }
 
 
 
-export Type Model = {
+export type Model = {
   _id: string
   _name: string
   _icon: string
   
 }
 
-export Type PropertyTypes = 'string' | 'date' |  'datetime' | 'timestamp' | 'markdown' | 'json'
-export Type Property = PropertyTypes | `[${PropertyTypes}]` | `${PropertyTypes}!` 
+export type PropertyTypes = 'string' | 'date' |  'datetime' | 'timestamp' | 'markdown' | 'json'
+export type Property = PropertyTypes | `[${PropertyTypes}]` | `${PropertyTypes}!` 
 
 export const database = init => {
   const { _id, _name, _icon = '■', ...collections } = init
   return {
     id: _id ?? init._name ? slugify(init._name) : Math.random().toString(36).substring(2,7),
     name,
-    icon,
+    icon: _icon,
     collections
   }
 }
+
+const slugify = text => text.toLowerCase().replace(/ /g,'-').replace(/[^\w-]+/g,'')
