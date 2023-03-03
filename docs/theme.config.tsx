d36c14f62e7987e137057188c347fe04bbf07e98
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2023-03-03 03:02:44 -0600

    Refactored folders and added docs

---
+++ docs/theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>My Project</span>,
+  project: {
+    link: 'https://github.com/graphdl/graphdl',
+  },
+  chat: {
+    link: 'https://discord.gg/NhYuABP9',
+  },
+  docsRepositoryBase: 'https://github.com/graphdl/graphdl/tree/master/docs',
+  footer: {
+    text: 'GraphDL.org',
+  },
+}
+
+export default config
