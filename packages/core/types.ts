21a8239dd6826955adc1a660d7865c327b22e0f7
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2023-11-05 14:38:01 -0600

    Added ExtractPredicates type

--- packages/core/types.ts
+++ packages/core/types.ts
@@ -1,7 +1,7 @@
 import { SchemaThings, SchemaActions, SchemaProperties } from './schema'
 
 export type DefaultNouns = 'User' | 'Trigger' | 'Action' | 'Noun' | 'Verb' | 'Event' | 'Property' | 'Resource' | 'App' | 'API' | 'UI' | 'Function' | 'Request' | 'Response'
-export type CommonPrepositions = 'about' | 'as' | 'at' | 'by' | 'for' | 'from' | 'in' | 'of' | 'on' | 'to' | 'via' | 'with' | 'within'
+export type CommonPrepositions = 'about' | 'after' | 'as' | 'at' | 'before' | 'by' | 'for' | 'from' | 'in' | 'of' | 'on' | 'to' | 'via' | 'with' | 'within'
 export type ExtendedPrepositions = 'aboard' | 'about' | 'above' | 'across' | 'after' | 'against' | 'along' | 'amid' | 'among' | 'anti' | 'around' | 'as' | 'at' | 'before' | 'behind' | 'below' | 'beneath' | 'beside' | 'besides' | 'between' | 'beyond' | 'but' | 'by' | 'concerning' | 'considering' | 'despite' | 'down' | 'during' | 'except' | 'excepting' | 'excluding' | 'following' | 'for' | 'from' | 'in' | 'inside' | 'into' | 'like' | 'minus' | 'near' | 'of' | 'off' | 'on' | 'onto' | 'opposite' | 'outside' | 'over' | 'past' | 'per' | 'plus' | 'regarding' | 'round' | 'save' | 'since' | 'than' | 'through' | 'to' | 'toward' | 'towards' | 'under' | 'underneath' | 'unlike' | 'until' | 'up' | 'upon' | 'versus' | 'via' | 'with' | 'within' | 'without'
 
 export type Things<N extends Nouns<N>> = SchemaThings | DefaultNouns | NounNames<N>
@@ -12,24 +12,27 @@ export type Nouns<N extends Record<string, any>> = {
   [K in keyof N]: Things<N> | Is | Noun<N[K]> | Array<Things<N> | PropositionalPhrase<N>>
 }
 
-// Verb - Creates
-// Action - Create
-// Trigger - onCreate
-// Activity - Creating
-// Event - Created
-
 export type Is = { is: any }
 export type PresentTense = `${string}s`
+// TODO: Add support for irregular verbs with overrides in the verbs object for Action, Trigger, Activity, and Event
+export type Verbs<N extends Nouns<N>> = Partial<Record<keyof Nouns<N>, Record<string, keyof Nouns<N> | Partial<Record<keyof Nouns<N>, Partial<Record<ExtendedPrepositions, Things<N>>>>>>>>
+
+export type ExtractPredicates<V extends Verbs<N>, N extends Nouns<N>> = {
+  [Subject in keyof V]: V[Subject] extends Record<string, any> ? keyof V[Subject] : never
+}[keyof V]
+
 export type ToAction<V extends string> = V extends `${infer A}s` ? A : V
 export type ToTrigger<V extends string> = V extends `${infer A}s` ? `on${Capitalize<A>}` : V
 export type ToActivity<V extends string> = V extends `${infer A}s` ? `${A}ing` : V
 export type ToEvent<V extends string> = V extends `${infer A}s` ? `${A}ed` : V
 
+export const createGraph = <N extends Nouns<N>, V extends Verbs<N>>(nouns: N, verbs?: V) => ({ nouns, verbs })
 
-export const createGraph = <N extends Nouns<N>>(nouns: N, verbs?: Verbs<N>) => ({ nouns, verbs })
-
-// export type Verbs<N extends Nouns<N>> = Partial<Record<keyof Nouns<N>, Record<string, keyof Nouns<N>>>>
-export type Verbs<N extends Nouns<N>> = Partial<Record<keyof Nouns<N>, Record<string, keyof Nouns<N> | Partial<Record<keyof Nouns<N>, Partial<Record<ExtendedPrepositions, Things<N>>>>>>>>
+// Verb - Creates
+// Action - Create
+// Trigger - onCreate
+// Activity - Creating
+// Event - Created
 
 export const Automotive = createGraph({
   Vehicle: 'Vehicle',
