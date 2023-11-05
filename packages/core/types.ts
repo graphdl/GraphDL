fe2e61d6e7cd489ea6a3452b42f4896093cb71cf
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2023-11-05 17:20:59 -0600

    Progress with SaaS example

--- packages/core/types.ts
+++ packages/core/types.ts
@@ -4,15 +4,16 @@ export type DefaultNouns = 'User' | 'Trigger' | 'Action' | 'Noun' | 'Verb' | 'Ev
 export type CommonPrepositions = 'about' | 'after' | 'as' | 'at' | 'before' | 'by' | 'for' | 'from' | 'in' | 'of' | 'on' | 'to' | 'via' | 'with' | 'within'
 export type ExtendedPrepositions = 'aboard' | 'about' | 'above' | 'across' | 'after' | 'against' | 'along' | 'amid' | 'among' | 'anti' | 'around' | 'as' | 'at' | 'before' | 'behind' | 'below' | 'beneath' | 'beside' | 'besides' | 'between' | 'beyond' | 'but' | 'by' | 'concerning' | 'considering' | 'despite' | 'down' | 'during' | 'except' | 'excepting' | 'excluding' | 'following' | 'for' | 'from' | 'in' | 'inside' | 'into' | 'like' | 'minus' | 'near' | 'of' | 'off' | 'on' | 'onto' | 'opposite' | 'outside' | 'over' | 'past' | 'per' | 'plus' | 'regarding' | 'round' | 'save' | 'since' | 'than' | 'through' | 'to' | 'toward' | 'towards' | 'under' | 'underneath' | 'unlike' | 'until' | 'up' | 'upon' | 'versus' | 'via' | 'with' | 'within' | 'without'
 
-export type Things<N extends Nouns<N>> = SchemaThings | DefaultNouns | NounNames<N>
+export type Things<N extends Nouns<N>> = Possibly<SchemaThings | DefaultNouns | NounNames<N>>
 export type PropositionalPhrase<N extends Nouns<N>> = `${CommonPrepositions}${Things<N>}` | `not${Capitalize<CommonPrepositions>}${Things<N>}`
 export type NounNames<N extends Nouns<N>> = keyof Nouns<N> extends string ? keyof Nouns<N> : never
 export type Noun<N extends Nouns<N>> = Record<ExtendedPrepositions | 'is', Things<N> | Array<Things<N>>> & Is
 export type Nouns<N extends Record<string, any>> = {
-  [K in keyof N]: Things<N> | Is | Noun<N[K]> | Array<Things<N> | PropositionalPhrase<N>>
+  [K in keyof N]: Things<N> | Is | Noun<N[K]> | Array<Things<N>>  // | PropositionalPhrase<N>> Commenting this out for now because it's causing performance issues
 }
 
 export type Is = { is: any }
+export type Possibly<N extends string> = N | `${N}?`
 export type PresentTense = `${string}s`
 // TODO: Add support for irregular verbs with overrides in the verbs object for Action, Trigger, Activity, and Event
 export type Verbs<N extends Nouns<N>> = Partial<Record<keyof Nouns<N>, Record<string, keyof Nouns<N> | Partial<Record<keyof Nouns<N>, Partial<Record<ExtendedPrepositions, Things<N>>>>>>>>
