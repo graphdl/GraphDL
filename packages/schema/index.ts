ed51362e275691d09cebf3075a09f94e4204e2f6
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2023-11-13 11:29:25 -0600

    Trying to rethink statements

--- packages/schema/index.ts
+++ packages/schema/index.ts
@@ -1,40 +1,26 @@
 import { Things, BasePrepositions } from './things'
 
 
-export type Prepositions = 
-  'in' |  'on' |  'at' |  'from' |  'to' |  'with' |  'by' |  'about' |  'for' |  'of' |  'over' |  'under' |  'through' |  
-  'during' |  'before' |  'after' |  'between' |  'among'
-
-export type PrepositionalPhrase = `${Prepositions}${Things}`
-
 // export type Things = 'Dataset' | 'EntryPoint' | 'WebAPI' 
+export type Prepositions = 'in' |  'on' |  'at' |  'as' | 'from' |  'to' |  'with' |  'by' |  'about' |  'for' |  'of' |  'over' |  'under' |  'through' |  'during' |  'before' |  'after' |  'between' |  'among'
+
 export type IsThing = `is${Things}`
-
-// const result = Schema()
-//   .API.isWebAPI()
-//   .Endpoint.isEntryPoint()
-//   .Resource.isDataset()
-
-// expect(result).toEqual({
-//   API: { is: 'WebAPI' },
-//   Endpoint: { is: 'EntryPoint' },
-//   Resource: { is: 'Dataset' }
-// })
+export type PrepositionThing = `${BasePrepositions}${Things}`
+export type PrepositionalPhrase = {
+  [P in PrepositionThing]: PrepositionalPhrase
+} & ReturnStatement
+export type Statement = Record<IsThing, PrepositionalPhrase>
+export type StatementObject = {
+  [K in 'is' | BasePrepositions]: Things
+} & ReturnStatement
+export type ReturnStatement = () => StatementObject
 
 
-const schemaHandler = {
-  get(target: { [x: string]: any }, prop: string) {
-    if (prop === 'toJSON') { // This method will help in the final conversion to the expected result format
-      return () => target;
-    }
-
-    // Return a function that will handle the 'isThing' methods
-    return {
-      isThing: (thing: Things) => {
-        target[prop] = { is: thing };
-        return new Proxy(target, schemaHandler); // Return a new Proxy with updated state
-      }
-    }[prop] || target[prop];
+const crm: Statement = new Proxy({} as Statement, {
+  get(target: Statement, prop: string) {
+    console.log(prop)
   }
-}
-export const Schema = () => new Proxy({}, schemaHandler)
+})
+
+crm.isWebApplication
+expect(crm.isSoftwareApplication.asService.forUseAction.ofOrganization()).toMatchInlineSnapshot()
